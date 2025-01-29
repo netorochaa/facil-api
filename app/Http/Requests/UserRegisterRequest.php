a376ca71508aae30eef3adc63af4e86615a20b75
netorochaa <jrr.neto@hotmail.com>
2025-01-29 12:16:42 -0300

    translate user register validations

--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -11,7 +11,23 @@ class UserRegisterRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'password' => ['required', 'string', 'min:6'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório.',
+            'name.string' => 'O campo nome deve ser uma string.',
+            'name.max' => 'O campo nome não pode ter mais de 255 caracteres.',
+            'email.required' => 'O campo email é obrigatório.',
+            'email.email' => 'O campo email deve ser um endereço de email válido.',
+            'email.max' => 'O campo email não pode ter mais de 255 caracteres.',
+            'email.unique' => 'O email fornecido já está em uso.',
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.string' => 'O campo senha deve ser uma string.',
+            'password.min' => 'O campo senha deve ter pelo menos 6 caracteres.',
         ];
     }
 }
