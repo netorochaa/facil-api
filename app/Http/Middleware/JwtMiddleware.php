81e3ca2697c1db28ac402432970c2df5187a6480
netorochaa <jrr.neto@hotmail.com>
2025-01-28 21:37:01 -0300

    Add jwt routes

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JwtMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Token not valid'], 401);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\Api\JWTAuthController;
+use App\Http\Middleware\JwtMiddleware;
+use Illuminate\Support\Facades\Route;
+
+Route::post('register', [JWTAuthController::class, 'register']);
+Route::post('login', [JWTAuthController::class, 'login']);
+
+Route::middleware([JwtMiddleware::class])->group(function () {
+    Route::get('user', [JWTAuthController::class, 'getUser']);
+    Route::post('logout', [JWTAuthController::class, 'logout']);
+});
