0eccf4fda5e14c8274052bfabfaff1ca0e953231
netorochaa <jrr.neto@hotmail.com>
2025-01-28 21:33:42 -0300

    Create jwt controller

---
+++ app/Http/Controllers/Auth/Api/JWTAuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTAuthController extends Controller
+{
+    public function register(UserRegisterRequest $request): JsonResponse
+    {
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+
+            $user = auth()->user();
+
+            return response()->json(compact('token'));
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Não foi possível criar o token. Erro: '.$e->getMessage()], 500);
+        }
+    }
+
+    public function getUser(): JsonResponse
+    {
+        try {
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['error' => 'Usuário não encontrado.'], 404);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Token inválido'], 400);
+        }
+
+        return response()->json(compact('user'));
+    }
+
+    public function logout(): JsonResponse
+    {
+        JWTAuth::invalidate(JWTAuth::getToken());
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
